395fa4fe3ec1a33b97d161aa0c68fc8b260fd9e9
Henrik Jürges <juerges.henrik@gmail.com>
2016-12-01 15:40:57 +0100

    added template delta to sparql generator

---
+++ src/main/java/de/hpi/rdf/tailrapi/TlrParser.java
@@ -0,0 +1,14 @@
+package de.hpi.rdf.tailrapi;
+
+/**
+ * This class provides static function to convert
+ * Deltas into different formats.
+ *
+ * Created by Henrik Jürges (juerges.henrik@gmail.com)
+ */
+public class TlrParser {
+
+    public static String convertToBasicSparql(Delta d, String graph) {
+        return "";
+    }
+}
---
+++ src/test/java/de/hpi/rdf/tailrapi/TlrParserTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by ratzeputz on 01.12.16.
+ */
+public class TlrParserTest {
+
+}
